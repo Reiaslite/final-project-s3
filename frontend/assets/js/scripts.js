7e4d22de4c3a2ac6a7004a6ed0d9fd62f256898e
faisalProject <mf494284@gmail.com>
2022-11-13 00:17:33 +0700

    partial update

--- frontend/assets/js/scripts.js
+++ frontend/assets/js/scripts.js
@@ -3,4 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementsByClassName('hamburger-container')[0].addEventListener('click', () => {
         document.getElementsByClassName('container-menu')[0].classList.toggle('active');
     })
+
+    const bottoms = document.getElementsByClassName('bottom');
+    for (const bottom of bottoms) {
+        bottom.addEventListener('click', () => {
+            document.getElementsByClassName('candidate-pair-details')[0].classList.add('active');
+        })
+    }
+    
+    document.getElementsByClassName('close-container')[0].addEventListener('click', () => {
+        document.getElementsByClassName('candidate-pair-details')[0].classList.remove('active');
+    })
 })
